201bd5784c6f923d3f20119c6c1e86cacf7dac45
uroojbadar <uroojbadar9@gmail.com>
2023-03-05 18:06:51 +0500

    Add light mode in website.

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,43 +1,51 @@
+import React from 'react'
+import Proptypes from 'prop-types'
 
-function App() {
+function Navbar(props) {
     return (
       <>
      
-     <header class="header_wrapper">
-        <nav class="navbar navbar-expand-lg fixed-top">
-            <div class="container d-flex justify-content-end">
+     <header className="header_wrapper">
+        <nav className={`navbar navbar-expand-lg fixed-top bg-${props.mode}`}>
+            <div className="container d-flex justify-content-end">
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-stream navbar-toggler-icon"></i>
+                    <i className="fas fa-stream navbar-toggler-icon"></i>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav menu-navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav menu-navbar-nav ">
+                        <li className="nav-item">
+                            <a className="nav-link active" style={{color:props.mode==='light'?'black':'#c6c9d8bf'}} aria-current="page" href="#home">Home</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About</a>
+                        <li className="nav-item">
+                            <a className="nav-link"  style={{color:props.mode==='light'?'black':'#c6c9d8bf'}}href="#about">About</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#services">Services</a>
+                        <li className="nav-item">
+                            <a className="nav-link"  style={{color:props.mode==='light'?'black':'#c6c9d8bf'}}href="#services">Services</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#portfolio">Portfolio</a>
+                        <li className="nav-item">
+                            <a className="nav-link"  style={{color:props.mode==='light'?'black':'#c6c9d8bf'}}href="#portfolio">Portfolio</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="poem.html">Poems</a>
+                        <li className="nav-item">
+                            <a className="nav-link"  style={{color:props.mode==='light'?'black':'#c6c9d8bf'}}href="poem.html">Poems</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#videos">Videos</a>
+                        <li className="nav-item">
+                            <a className="nav-link"  style={{color:props.mode==='light'?'black':'#c6c9d8bf'}}href="#videos">Videos</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="blogs.html">Blogs</a>
+                        <li className="nav-item">
+                            <a className="nav-link"  style={{color:props.mode==='light'?'black':'#c6c9d8bf'}}href="blogs.html">Blogs</a>
                         </li>
-                        <li class="nav-item mt-3 mt-lg-0">
-                            <a>
-                                <button class="main-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#Contact">Contact</button>  
+                        <li className="nav-item mt-3 mt-lg-0">
+                            <a  href="/">
+                                <button className="main-btn" style={{ color: props.mode === 'light' ? 'black' : '#c6c9d8bf', borderColor: props.mode === 'light' ? 'black' : '#c6c9d8bf' }} type="button" data-bs-toggle="offcanvas" data-bs-target="#Contact">Contact</button>  
                             </a>
+                        </li>
+                        <li className="pt-2">
+                            <div class="m-check form-switch">
+                                <input className="form-check-input f-c" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                                <label className="form-check-label p-1" htmFor="flexSwitchCheckDefault" style={{color:props.mode==='light'?'black':'#c6c9d8bf'}}>Light Mode</label>
+                            </div>
                         </li>
 
                     </ul>
@@ -50,6 +58,6 @@ function App() {
     );
   }
   
-  export default App;
+  export default Navbar;
   
   
